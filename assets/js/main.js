c68e38ce20c094b578ffb2d3ec13b33a7a0a1104
Anayn Pavez <nanyn.pavez@gmail.com>
2017-06-06 23:41:51 -0300

    nav no funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
-var navegador = document.getElementById('nav');
 
-window.onscroll = function(){
-	"use strict";
-	if(document.body.scrollTop >= 280){
+
+window.addEventListener = function(){
+ 	const bajada = window.pageYOffset || document.documentElement.scrollTop,
+ 	navegador = document.getElementById('nav');
+
+	if(bajada > 60){
 		navegador.classList.add('scroll');
+		document.getElementById('primero').classList.add('');
+		document.getElementById('segundo').classList.add('');
+		document.getElementById('tercero').classList.add('');
+		document.getElementById('cuarto').classList.add('');
+		document.getElementById('quinto').classList.add('');
 	} else{
 		navegador.classList.remove('scroll');
+		document.getElementById('').classList.remove('');
+
 	}
 };
